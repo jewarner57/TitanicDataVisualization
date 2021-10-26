c3abfb6129d8e7fa149f8d37c86041427d4e9154
Jonathan Warner <jewarner57@gmail.com>
2021-10-25 20:29:56 -0400

    feat: add select and refactor code to allow for filtering displayed data

--- script.js
+++ script.js
@@ -18,23 +18,34 @@ const passengers = data.map(p => {
   return document.createElement('div')
 })
 
-// Loop over each passenger and append them to the titanic
-passengers.forEach(p => {
-  titanic.appendChild(p)
+// Get the select element and add an event listener
+const selectElement = document.querySelector('#filterBy')
+let filterBy = selectElement.value
+displayData(filterBy)
+
+selectElement.addEventListener('input', (e) => {
+  filterBy = e.target.value
+
+  displayData(filterBy)
 })
 
-// Let's loop over each passenger and set some styles 
-passengers.forEach((p, i) => {
-  const { fare, name, embarked, pclass, sex, survived } = data[i].fields
-  const embarkedColorMap = { 'S': '#324376', 'C': '#F76C5E', 'Q': '#F4CC48' }
+function displayData(filterBy) {
 
-  p.style.margin = '0.5px'
-  p.style.width = '15px'
-  p.style.height = '15px'
-  p.style.backgroundColor = embarkedColorMap[embarked]
-  p.style.borderRadius = sex === 'male' ? '0' : '50%'
-  p.style.opacity = survived === 'Yes' ? '1' : '0.5'
-})
+  // Loop over each passenger and append them to the titanic
+  passengers.forEach(p => {
+    titanic.appendChild(p)
+  })
 
+  // Let's loop over each passenger and set some styles 
+  passengers.forEach((p, i) => {
+    const { fare, name, embarked, pclass, sex, survived } = data[i].fields
+    const embarkedColorMap = { 'S': '#324376', 'C': '#F76C5E', 'Q': '#F4CC48' }
 
-
+    p.style.margin = '0.5px'
+    p.style.width = '15px'
+    p.style.height = '15px'
+    p.style.backgroundColor = embarkedColorMap[embarked]
+    p.style.borderRadius = sex === 'male' ? '0' : '50%'
+    p.style.opacity = survived === 'Yes' ? '1' : '0.5'
+  })
+}
