b84d1f859cd109ddc4f8ff8b18e3b682e9fe6334
Jonathan Warner <jewarner57@gmail.com>
2021-10-29 19:44:35 -0400

    feat: style page, fix numeric value sorting issues, add info key, add passenger view

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const pageWrapper = document.querySelector('#pageWrapper')
 const filterControls = document.querySelector('#filterControls')
 
 // Set filter controls styles
-filterControls.style.padding = '50px'
+filterControls.style.padding = '0 50px 0 50px'
 
 // Set page wrapper styles
 pageWrapper.style.width = '100%'
 pageWrapper.style.display = 'flex'
 pageWrapper.style.flexDirection = 'row'
 pageWrapper.style.justifyContent = 'center'
+pageWrapper.style.flexWrap = 'wrap'
 
 // Set some styles on the titanic
 // display flex, justifyContent center, alignItems flex-end
@@ -19,6 +20,7 @@ titanic.style.display = 'flex'
 titanic.style.flexDirection = 'row'
 titanic.style.flexWrap = 'wrap'
 titanic.style.maxWidth = '50vw'
+titanic.style.minWidth = '400px'
 
 // Map over the data and make a new element for each passenger
 const passengers = data.map(p => {
@@ -29,7 +31,7 @@ let selectedFilters = []
 
 // Get the select element and display the initial data
 const selectElement = document.querySelector('#filterBy')
-const filteredData = filterData('age', data)
+const filteredData = filterData('survived', data)
 selectElement.addEventListener('input', selectChanged)
 displayData(filteredData)
 
@@ -53,11 +55,12 @@ addFilterButton.addEventListener('click', (e) => {
 removeFilterButton.addEventListener('click', (e) => {
   const elemToRemove = filterList.children[[...filterList.children].length - 1]
   filterList.removeChild(elemToRemove)
+
+  selectChanged()
 })
 
-function selectChanged(e) {
+function selectChanged() {
   selectedFilters = [...filterList.children].map((elem) => {
-    console.log(elem.value)
     return elem.value
   })
 
@@ -72,7 +75,10 @@ function selectChanged(e) {
 }
 
 function displayPassengerData(e) {
-  console.log(e.target.passengerData)
+  const detailDisplay = document.querySelector('#passengerDetail')
+  const dataObj = Object.entries(e.target.passengerData)
+
+  detailDisplay.innerHTML = String(dataObj.map((item, index) => `<p><b>${item[0]}:</b> ${item[1] || 'No Data'}</p>`)).split(",").join("")
 }
 
 /* Build the person list graphic */
@@ -86,19 +92,23 @@ function displayData(passengerData) {
   // Let's loop over each passenger and set some styles 
   passengers.forEach((p, i) => {
     const { fare, name, embarked, pclass, sex, survived, age } = passengerData[i].fields
+    // Southhampton (England), Cherbourg (France), Queenstown (Ireland)
     const embarkedColorMap = { 'S': '#4462b9', 'C': '#F76C5E', 'Q': '#F4CC48' }
 
     p.innerHTML = age < 1 ? "<1" : Math.round(age) || "-"
 
+    p.style.transition = 'all 10s ease'
     p.style.margin = '0.5px'
     p.style.width = '15px'
     p.style.height = '15px'
+    p.style.fontSize = '14px'
     p.style.backgroundColor = embarkedColorMap[embarked] || '#fff'
     p.style.borderRadius = sex === 'male' ? '0' : '50%'
     p.style.opacity = survived === 'Yes' ? '1' : '0.3'
     p.style.textAlign = 'center'
     p.style.padding = pclass === 1 ? '7px' : '9px'
     p.style.border = pclass === 1 ? '2px solid black' : ''
+
 
     p.passengerData = { fare, name, embarked, pclass, sex, survived, age }
     p.addEventListener('click', displayPassengerData)
@@ -113,11 +123,21 @@ function filterData(filterBy, data) {
   switch (filterBy) {
     case 'age':
       // Age: Oldest at the top
-      filteredData.sort((a, b) => b.fields.age - a.fields.age)
+      filteredData.sort((a, b) => {
+        ageA = a.fields.age || -1
+        ageB = b.fields.age || -1
+
+        return ageB - ageA
+      })
       break;
     case 'fare':
       // Fares: Highest at the top
-      filteredData.sort((a, b) => b.fields.fare - a.fields.fare)
+      filteredData.sort((a, b) => {
+        fareA = a.fields.fare || -1
+        fareB = b.fields.fare || -1
+
+        return fareB - fareA
+      })
       break;
     case 'sex':
       // Sex: Males at the top
